e5ec5574bd11c335f839e2b978724fb6c3e48eb3
keddyboys <keddyboys@gmail.com>
2016-02-03 21:41:13 +0200

    Added locales for forum and Romanian locales for install!

---
+++ forum_subcategories/files/install/locale/Romanian.php
@@ -0,0 +1,13 @@
+<?php
+$locale['install_01'] = "Sub-forumuri";
+$locale['install_02'] = "Acest script va instala sub-forumurile.";
+$locale['install_03'] = "Instaleaz%#259;";
+$locale['install_04'] = "Acest script va dezinstala sub-forumurile.";
+$locale['install_05'] = "Deznstaleaz%#259;";
+$locale['install_06'] = "E&#351;ti sigur c%#259; vrei s%#259; dezinstalezi sub-forumurile?";
+$locale['install_07'] = "Rezultat";
+$locale['install_08'] = "Mod instalat cu succes! &#350;terge imediat acest director!";
+$locale['install_09'] = "Mod dezinstalat cu succes! &#350;terge imediat acest director!<br />Pune fis%#351;ierele originale pe host!";
+$locale['install_10'] = "ATEN&#354;IE!: Trebuie sa fii Super Administrator!";
+$locale['install_11'] = "Revenire la sait";
+?>
---
+++ forum_subcategories/files/locale/German/admin/forums.php
@@ -0,0 +1,83 @@
+<?php
+// Forum Category titles
+$locale['400'] = "Forum Kategorie hinzuf&uuml;gen";
+$locale['401'] = "Forum kategorie bearbeiten";
+// Forum Category messages
+$locale['410'] = "Forum Kategorie hinzugef&uuml;gt";
+$locale['411'] = "Forum Kategorie bearbeitet";
+$locale['412'] = "Die Forum Kategorie kann nicht gel&ouml;scht werden";
+$locale['413'] = "Es gibt noch Foren in dieser kategorie";
+$locale['414'] = "Forum Kategorie gel&ouml;scht";
+// Forum Category form
+$locale['420'] = "Kategorie Name";
+$locale['421'] = "Sortierung";
+$locale['422'] = "Kategorie speichern";
+// Delete Forum Category
+$locale['440'] = "Forum kategorie l&ouml;schen?";
+// Forum titles
+$locale['500'] = "Forum hinzuf&uuml;gen";
+$locale['501'] = "Forum bearbeiten";
+// Forum Category messages
+$locale['510'] = "Forum hinzugef&uuml;gt";
+$locale['511'] = "Forum aktualisiert";
+$locale['512'] = "Das Forum kann nicht gel&ouml;scht werden";
+$locale['513'] = "Es gibt noch Themen in diesem Forum";
+$locale['514'] = "Forum gel&ouml;sscht";
+$locale['515'] = "Forum Moderatoren aktualisiert";
+$locale['516'] = "Bitte einen Kategorie Namen eingeben.";
+$locale['517'] = "Bitte ein Foren Namen eingeben.";
+// Add/Edit Forum Form
+$locale['519'] = "&uuml;bergeodnetes Forum:";//subforums
+$locale['520'] = "Forum Name:";
+$locale['521'] = "Forum Beschreibung:";
+$locale['522'] = "Forum Kategorie:";
+$locale['523'] = "Sortierung:";
+$locale['524'] = "Foren Privilegien";
+$locale['525'] = "Forum sichtbar f&uuml;r:";
+$locale['526'] = "Themen er&ouml;ffnen:";
+$locale['527'] = "Beitr&auml;ge beantworten:";
+$locale['528'] = "Anh&auml;nge hinzuf&uuml;gen:";
+$locale['529'] = "Umfragen er&ouml;ffnen:";
+$locale['530'] = "Abstimmen:";
+$locale['531'] = "aus";
+$locale['532'] = "Forum speichern";
+$locale['533'] = "Forum Moderator";
+$locale['534'] = "Forum Moderator speichern";
+$locale['535'] = "Anh&auml;nge herunterladen:";
+// Forum Settings
+$locale['540'] = "Forum Einstellungen:";
+$locale['541'] = "Post Merger:";
+$locale['542'] = "aktiviert";
+$locale['543'] = "deaktiviert";
+// Current Forums
+$locale['550'] = "Current Forums";
+$locale['551'] = "Kategorie/Forum";
+$locale['552'] = "Sortierung";
+$locale['553'] = "Optionen";
+$locale['554'] = "bearbeiten";
+$locale['555'] = "l&ouml;schen";
+$locale['556'] = "rauf";
+$locale['557'] = "runter";
+$locale['558'] = "nach oben verschieben";
+$locale['559'] = "nach unten verschieben";
+$locale['560'] = "Keine kategorien definiert";
+$locale['561'] = "keine Foren difiniert";
+$locale['562'] = "Foren Sortierung erneuern";
+$locale['563'] = "bereinigen";
+// Delete Forum
+$locale['570'] = "Forum l&ouml;schen?";
+// Pruning
+$locale['600'] = "Forum bereinigen";
+$locale['601'] = "Diese Prozedur wird Themen entfernen";
+$locale['602'] = "die &auml;lter sind als der ausgew&auml;hlte Zeitraum.";
+$locale['603'] = "Bereinigungszeitraum: ";
+$locale['604'] = "Woche";
+$locale['605'] = "Wochen";
+$locale['606'] = "Monat";
+$locale['607'] = "Monate";
+$locale['608'] = "Foren bereinigung abgeschlossen.";
+$locale['609'] = "Beitr&auml;ge gel&ouml;scht: ";
+$locale['610'] = "Anh&auml;nge gel&ouml;scht: ";
+$locale['611'] = "Themen entfernt: ";
+$locale['612'] = "zu bereinigende Themen und Beitr&auml;ge werden gel&ouml;scht.Willst du fortfahren?";
+?>
