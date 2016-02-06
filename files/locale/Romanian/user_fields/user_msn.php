294c3416a39cd853b109ef6da96143f67993f86b
keddyboys <keddyboys@gmail.com>
2016-02-06 22:42:44 +0200

    Delete

--- files/locale/Romanian/user_fields/user_msn.php
+++
@@ -1,5 +0,0 @@
-<?php
-$locale['uf_msn'] = "MSN";
-$locale['uf_msn_desc'] = "MSN Messenger";
-$locale['uf_msn_error'] = "Eroare: C&#226;mpul 'MSN' nu poate fi l&#259;sat gol.";
-?>
--- files/locale/Romanian/user_fields/user_yahoo.php
+++
@@ -1,5 +0,0 @@
-<?php
-$locale['uf_yahoo'] = "Yahoo!";
-$locale['uf_yahoo_desc'] = "Yahoo! Instant Messenger";
-$locale['uf_yahoo_error'] = "Eroare: C&#226;mpul 'Yahoo' nu poate fi l&#259;sat gol.";
-?>
